74c7d3eebcd58342954067372645cfdb6f0c67b0
ayman_bentouhami <ali-17baba@outlook.fr>
2024-01-24 16:22:33 +0100

    insert order finished

--- Code/App.py
+++ Code/App.py
@@ -1,6 +1,6 @@
-from establish_conn import conn
 
-from flask import Flask,render_template,jsonify
+import pyodbc
+from flask import Flask,render_template,jsonify, request
 #import pandas as pd
 #import requests
 #from sklearn.svm import SVC
@@ -15,6 +15,43 @@ def home():
     template_path = 'home.html'
     return render_template(template_path,name='Home')
 
+
+@app.route('/insertOrder', methods=['POST'])
+def insertOrder():
+    conn_str = (
+    r'Driver={SQL Server};'
+    r'Server=DESKTOP-A4UCBC2;'
+    r'Database=DATA_BASE;'
+    r'Trusted_Connection=yes;'
+    )
+    conn = pyodbc.connect(conn_str)
+
+    try:
+        dataToSend = request.json
+        try:
+            dataToSend['NBHeuresMOValue'] = float(dataToSend['NBHeuresMOValue'])
+        except:
+            return "NBHeuresMOValue should be a float"
+        try:
+            dataToSend['IDOrdre'] = int(dataToSend['IDOrdre'])
+        except:
+            return "IDOrdre should be a digit"
+        try:
+            cursor = conn.cursor()
+            cursor.execute("USE DATA_BASE")
+            query = f"INSERT INTO ORDREREPARATION(DiagnosticPanne, NbHeuresMO, IDApp) Values ('{dataToSend['diagnosticPanne']}',{dataToSend['NBHeuresMOValue']},{dataToSend['IDOrdre']})"
+            print(query)
+            cursor.execute(query)
+            cursor.commit()
+        except Exception as e:
+            return e
+
+    except:
+        print("error")
+
+    
+    return "Ordre inserted succesfully"
+        
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
--- Code/static/index.js
+++ Code/static/index.js
@@ -8,23 +8,82 @@ document.addEventListener("DOMContentLoaded", function() {
         var heading1 = document.createElement("h1");
         heading1.innerText = 'Inserer un Ordre de Reparation';
 
-        var input_div1 = document.createElement('div')
+        var input_div1 = document.createElement('div');
+        var input_div2 = document.createElement('div');
+        var input_div3 = document.createElement('div');
 
-        var input_label1 = document.createElement('label')
-        var input1 = document.createElement('input')
-        input_label1.innerText = "diagnosticPanne"
-        input1.type = 'text'
+        var input_label1 = document.createElement('label');
+        var input1 = document.createElement('input');
+        input_label1.innerText = "diagnosticPanne:";
+        input1.type = 'text';
+
+        var input_label2 = document.createElement('label');
+        var input2 = document.createElement('input');
+        input_label2.innerText = "NBHeuresMO:";
+        input2.type = 'text';
+
+        var input_label3 = document.createElement('label');
+        var input3 = document.createElement('input');
+        input_label3.innerText = "IDOrdre:";
+        input3.type = 'text';
+
+        var error_label = document.createElement('label')
 
 
 
-        input_div1.appendChild(input_label1)
-        input_div1.appendChild(input1)
-        insertion_div.appendChild(heading1)
-        insertion_div.appendChild(input_div1)
+        var submit = document.createElement('button')
+        submit.innerText = "Submit"
+
+        input_div1.appendChild(input_label1);
+        input_div1.appendChild(input1);
+
+        input_div2.appendChild(input_label2);
+        input_div2.appendChild(input2);
+
+        input_div3.appendChild(input_label3);
+        input_div3.appendChild(input3);
+
+        insertion_div.appendChild(heading1);
+        insertion_div.appendChild(input_div1);
+        insertion_div.appendChild(input_div2);
+        insertion_div.appendChild(input_div3);
+        insertion_div.appendChild(submit)
+        insertion_div.appendChild(error_label)
+
+        
+        heading1.style.marginBottom = '20px'; 
+        input_div1.style.marginBottom = '10px'; 
+        input_div2.style.marginBottom = '10px'; 
+         
+        submit.addEventListener("click", function(){
+            var diagnosticPanneValue = input1.value;
+            var NBHeuresMOValue = input2.value;
+            var IDOrdre = input3.value;
+
+            dataToSend = {
+                diagnosticPanne: diagnosticPanneValue,
+                NBHeuresMOValue: NBHeuresMOValue,
+                IDOrdre: IDOrdre
+            }
+
+            fetch('/insertOrder', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataToSend)
+            })
+            .then(response => response.text())
+            .then(data => {
+                console.log('Response from Flask:', data);
+                // Handle the response as needed
+                error_label.innerText = data
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        });
     }
 
-
-
     reparationInsertForum();
-
 });
